176a390238a4cd9d32933f29ca9d7ea3cd12048a
VuralAktas <vaktas19@ku.edu.tr>
2024-12-09 10:15:57 +0300

    Exe Local Time Error Debug

--- main_APP.py
+++ main_APP.py
@@ -1,9 +1,14 @@
 import tkinter as tk
 
 from app import Main_Section
+import locale
 
 
 def main():
+    try:
+        locale.setlocale(locale.LC_ALL, '')
+    except locale.Error:
+        locale.setlocale(locale.LC_ALL, 'C')
     ROOT = tk.Tk()
     Main_Section(ROOT)
     ROOT.mainloop()
